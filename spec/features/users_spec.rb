58206d6644e0e479051f5b4aaa70e8917d8db929
flakster <carlos.santamaria@gmail.com>
2020-05-29 15:45:58 -0500

    Fix offenses reported by rubocop

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 # rubocop:disable Metrics/BlockLength
 require 'rails_helper'
 
-RSpec.feature "Users", type: :feature do
+RSpec.feature 'Users', type: :feature do
   context 'user friendship requests' do
     before(:each) do
-      u1 = User.create(name: 'User 1 test', email: 'user1@mail.com', password: '123456')
-      u2 = User.create(name: 'User 2 test', email: 'user2@mail.com', password: '123456')
-      u3 = User.create(name: 'User 3 test', email: 'user3@mail.com', password: '123456')
+      User.create(name: 'User 1 test', email: 'user1@mail.com', password: '123456')
+      User.create(name: 'User 2 test', email: 'user2@mail.com', password: '123456')
+      User.create(name: 'User 3 test', email: 'user3@mail.com', password: '123456')
       visit root_path
       within('form') do
         fill_in 'user_email', with: 'user2@mail.com'
@@ -20,12 +22,12 @@ RSpec.feature "Users", type: :feature do
     end
     scenario 'user can send a friendship request from the users list' do
       click_link 'All users'
-      first(:link, "Invite to friendship").click
+      first(:link, 'Invite to friendship').click
       expect(page).to have_content('Pending')
     end
     scenario 'user can send a friendship request from the user\'s profile' do
       click_link 'All users'
-      first(:link, "See Profile").click
+      first(:link, 'See Profile').click
       click_link 'Invite to friendship'
       expect(page).to have_content('Pending')
     end
@@ -34,7 +36,7 @@ RSpec.feature "Users", type: :feature do
     end
     scenario 'user can accept a friendship request' do
       click_link 'All users'
-      first(:link, "Invite to friendship").click
+      first(:link, 'Invite to friendship').click
       click_link 'Sign out'
       click_link 'Sign in'
       within('form') do
@@ -48,7 +50,7 @@ RSpec.feature "Users", type: :feature do
     end
     scenario 'user can reject a friendship request' do
       click_link 'All users'
-      first(:link, "Invite to friendship").click
+      first(:link, 'Invite to friendship').click
       click_link 'Sign out'
       click_link 'Sign in'
       within('form') do
@@ -61,6 +63,5 @@ RSpec.feature "Users", type: :feature do
       expect(page).to have_content('Notifications (0)')
     end
   end
-
 end
 # rubocop:enable Metrics/BlockLength
