97e1decae76a021a47792fbff3f2ce64cea5eda1
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-06-01 14:38:58 -0500

    Optimize the querys by impleting the creation of two rows per friendship.

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -7,6 +7,7 @@ class Friendship < ApplicationRecord
   scope :pending_friendships_request, lambda { |user_id|
     joins(:sender)
       .where(receiver_id: user_id, status: 'pending')
+      .where.not(sender_id: user_id)
       .select('friendships.id as friendship_id, users.id as sender_id, users.name as sender')
       .order('friendships.created_at asc')
   }
@@ -14,12 +15,12 @@ class Friendship < ApplicationRecord
   scope :pending_friendships_request_count, lambda { |user_id|
     joins(:sender)
       .where(receiver_id: user_id, status: 'pending')
+      .where.not(sender_id: user_id)
       .count
   }
 
   scope :friendship, lambda { |sender_id, receiver_id|
-    where("(sender_id = #{sender_id} and receiver_id = #{receiver_id})
-    or (sender_id = #{receiver_id} and receiver_id = #{sender_id})
-    ")
+    where(sender_id: sender_id)
+      .where(receiver_id: receiver_id)
   }
 end
