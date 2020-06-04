1b8a52065b346aac25977958580618274d1633f2
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-06-04 14:33:40 -0500

    Add omniauth login with github.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, 
+         :omniauthable, omniauth_providers: %i[github]
 
-  validates :name, presence: true, length: { maximum: 20 }
+  validates :name, presence: true, length: { maximum: 100 }
 
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
@@ -28,5 +29,23 @@ class User < ApplicationRecord
     Post.where(user: [friends.select(:id)])
       .or(Post.where(user: [id]))
       .order(created_at: :desc)
+  end
+
+  def self.from_omniauth(auth)
+    user = User.find_by(email: auth.info.email)
+    if user
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.save
+    else
+      user = User.new
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.save
+    end
+    user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'posts#index'
   resources :friendships, only: %i[create update]
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   get 'users/notifications'
   resources :users, only: [:index, :show, :notifications]
   resources :posts, only: [:index, :create] do
