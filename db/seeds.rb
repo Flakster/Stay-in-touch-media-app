60aca376e0f1fef0c6b50a70206c485b43109dae
flakster <carlos.santamaria@gmail.com>
2020-06-04 11:08:05 -0500

    Add Ajax behaviour to the friendship methods, and add the link to the heroku production version

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-100.times do |indx|
-    User.create(name: "User #{indx + 1}", email: "user_#{indx + 1}@mail.com", password: '123456')
-end
-
-90.times do |index|
-    Friendship.create(sender_id: index + 2, receiver_id: 1, status: 'pending')
-    Friendship.create(sender_id: 1, receiver_id: index + 2, status: 'sent')
-end
